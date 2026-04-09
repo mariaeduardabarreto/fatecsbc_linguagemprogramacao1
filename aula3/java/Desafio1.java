d43df8da583d4b96a894f34e9d6e2de717ad133e
mariaeduardabarreto <85494875+mariaeduardabarreto@users.noreply.github.com>
2026-04-08 23:19:41 -0300

    Desafio 1 Java

---
+++ aula3/java/Desafio1.java
@@ -0,0 +1,14 @@
+public class Desafio1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        String[] nomes = {"Ana", "João", "Maria", "Lucas", "Beatriz"};
+
+        for (String nome : nomes) {
+            System.out.println(nome);
+        }
+    }
+}
