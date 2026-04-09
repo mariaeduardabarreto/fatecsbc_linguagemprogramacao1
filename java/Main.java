aa63f335c9d7f0bda2ef59e02f9b26b90749b5d5
mariaeduardabarreto <85494875+mariaeduardabarreto@users.noreply.github.com>
2026-04-08 22:20:44 -0300

    Calculadora em Java

---
+++ java/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1, num2, resultado;
+        char operacao;
+
+        System.out.print("Digite o primeiro número: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Digite a operação (+, -, *, /): ");
+        operacao = scanner.next().charAt(0);
+
+        if (operacao == '+') {
+            resultado = num1 + num2;
+            System.out.println("Resultado: " + resultado);
+        } else if (operacao == '-') {
+            resultado = num1 - num2;
+            System.out.println("Resultado: " + resultado);
+        } else if (operacao == '*') {
+            resultado = num1 * num2;
+            System.out.println("Resultado: " + resultado);
+        } else if (operacao == '/') {
+            if (num2 != 0) {
+                resultado = num1 / num2;
+                System.out.println("Resultado: " + resultado);
+            } else {
+                System.out.println("Erro: divisão por zero");
+            }
+        } else {
+            System.out.println("Operação inválida");
+        }
+
+        scanner.close();
+    }
+}
