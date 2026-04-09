2fda3783d9b9d7664d11f3084052eccf8591613c
mariaeduardabarreto <85494875+mariaeduardabarreto@users.noreply.github.com>
2026-04-08 23:21:00 -0300

    Desafio 2 C++

---
+++ aula3/cpp/desafio2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero = 0;
+
+    while (numero <= 10) {
+        cout << "Digite um número: ";
+        cin >> numero;
+    }
+
+    cout << "Obrigado!" << endl;
+    return 0;
+}
